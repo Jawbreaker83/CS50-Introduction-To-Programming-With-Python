d43c4b8228500fae882ca1482c1552d683b96559
Doug Rubino <fewd85@gmail.com>
2023-09-12 16:45:17 -0400

    Conditionals

---
+++ Conditionals/house.py
@@ -0,0 +1,40 @@
+""" name = input('What Is Your Name? ')
+
+if name == 'Harry':
+    print('Gryffindor')
+elif name == 'Hermione':
+    print('Gryffindor')
+elif name == 'Ron':
+    print('Gryffindor')
+elif name == 'Draco':
+    print('Slytherin')
+else:
+    print('Who?') """
+
+##################################################
+""" 
+name = input('What Is Your Name? ')
+
+match name:
+    case "Harry":
+        print('Gryffindor')
+    case "Hermione":
+        print('Gryffindor')
+    case "Ron":
+        print('Gryffindor')
+    case "Draco":
+        print('Slytherin')
+    case _:
+        print('Who?') """
+
+##################################################
+
+name = input('What Is Your Name? ')
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print('Gryffindor')
+    case "Draco":
+        print('Slytherin')
+    case _:
+        print('Who?')
