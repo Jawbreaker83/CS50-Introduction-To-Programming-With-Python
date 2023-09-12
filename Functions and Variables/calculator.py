d7fd0dfbd6862c52d995a14fe2de8b2433c651d2
Doug Rubino <fewd85@gmail.com>
2023-09-12 11:05:14 -0400

    Functions Variables

---
+++ Functions and Variables/calculator.py
@@ -0,0 +1,25 @@
+""" x = input('What is x? ')
+y = input('What is y? ')
+
+z = int(x) + int(y)
+
+print(z) """
+
+###############################Another Way to Write The Code Above#################################################################################
+
+""" x = int(input('What is x? '))
+y = int(input('What is y? '))
+
+print( x + y) """
+
+###########################################################################################################
+
+def main():
+    x = int(input("What is x? "))
+    print('x squared is', square(x))
+
+def square(n):
+    # return n * n 
+    # return n ** 2
+    # return pow(n, 2)
+main ()
---
+++ Functions and Variables/float.py
@@ -0,0 +1,24 @@
+""" x = float(input('What is x? '))
+y = float(input('What is y? '))
+
+z = round(x + y)
+# f is the format string
+print(f'{z:,}')  """
+
+##############################################################
+
+""" x = float(input('What is x? '))
+y = float(input('What is y? '))
+
+z = round(x / y, 2)
+
+print(z) """
+
+####OR####################################
+
+x = float(input('What is x? '))
+y = float(input('What is y? '))
+
+z = x / y
+
+print(f'{z:.3f}')
