71ac344b42a2044e2d7a303108c95baf5f98cc64
Doug Rubino <fewd85@gmail.com>
2023-09-13 15:14:10 -0400

    Loops

---
+++ Loops/codeCat.py
@@ -0,0 +1,43 @@
+""" i = 0
+while i < 3:
+    print('meow')
+    i += 1 """
+
+############################################
+#  use the underscore the illustrate a variable for the sake of having to have a variable
+#  to loop
+""" for _ in range(3):
+    print('meow') """
+
+###############################################
+
+""" print('meow\n' *3, end="") """
+
+###############################################
+
+""" while True:
+    n = int(input('What is n? '))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print('meow')  """
+
+################################################
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n                
+  
+def meow(n):
+    for _ in range(n):
+        print('meow')
+
+main()
