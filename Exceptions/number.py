3750b520f72ef80ac029b8097d4880e82eff4d74
Doug Rubino <fewd85@gmail.com>
2023-09-14 14:55:19 -0400

    Exceptions

---
+++ Exceptions/number.py
@@ -0,0 +1,14 @@
+""" try:
+    x = int(input('What is x? '))
+    print(f'x is {x}')
+except ValueError:
+    print('x is not an integer') """
+
+    #############################################
+
+try:
+    x = int(input('What is x? '))
+except ValueError:
+    print('x is not an integer')
+else:
+    print(f'x is {x}')
