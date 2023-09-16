a889108a894857e1ffed3073649b1f5fb5b6980c
Doug Rubino <fewd85@gmail.com>
2023-09-16 10:21:18 -0400

    Libraries

---
+++ Libraries/sayings.py
@@ -0,0 +1,12 @@
+def main():
+    hello('world')
+    goodby('world')
+
+def hello(name):
+    print(f'hello, {name}')
+
+def goodby(name):
+    print(f'goodbye, {name}')
+
+if __name__ == '__main__':
+    main()
