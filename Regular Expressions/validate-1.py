bd4914db70c8d5d70d632560f1a1796734106ef5
Doug Rubino <fewd85@gmail.com>
2023-09-21 15:55:10 -0400

    Regular Expressions

--- Regular Expressions/validate-1.py
+++ Regular Expressions/validate-1.py
@@ -3,10 +3,12 @@ import re
 
 email = input("What is your email? ").strip()
 
-if re.search(r".+@.+\.edu", email):
+if re.search(r"^\w+@(\w+\.)?\w+\.(edu|com|gov|org)", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
+
+# [^@] means any character except an @ sign and the + to the right means as many or more can follow
 
     
 
